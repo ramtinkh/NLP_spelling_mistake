d9623418aa3de1b5785133f18f3be283230a10de
ramtinkh <ramtinkhoshnevis1@gmail.com>
2024-06-01 21:44:09 +0330

    release: final version added.

--- correction_skill.py
+++ correction_skill.py
@@ -1,18 +1,18 @@
 from email import message
 import logging
-from operator import mod
 import Levenshtein
 import jellyfish
-from datasets import load_dataset, Dataset
 import pandas as pd
 from opsdroid.skill import Skill
-from opsdroid.matchers import match_regex, match_parse
-from opsdroid.matchers import match_event, match_crontab, match_parse, match_regex
-from opsdroid.events import Message, JoinRoom, UserInvite, LeaveRoom
-
-from transformers import AutoTokenizer, AutoConfig, AutoModelWithLMHead
-from transformers import AutoTokenizer, GPT2LMHeadModel, GPT2Config, AutoModelForCausalLM
+from opsdroid.matchers import match_parse
+from opsdroid.matchers import match_event, match_parse, match_regex
+from opsdroid.events import JoinRoom, UserInvite
+from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+import pandas as pd
+import re
+
 
 class DistanceModel:
     def __init__(self, words_path):
@@ -24,9 +24,6 @@ class DistanceModel:
         with open(path, 'r', encoding='utf-8') as file:
             words = file.read().splitlines()
         return words
-
-    def load_words_df(self, path):
-        return pd.read_csv(path, sep='\t')
 
     def build_soundex_dict(self, words):
         soundex_dict = {}
@@ -52,44 +49,62 @@ class DistanceModel:
                 closest_word = persian_word
         return closest_word
 
-    def correct_words(self, words):
-        wrong_to_correct = dict(zip(self.words_df['wrong'], self.words_df['correct']))
-        corrected_words = [wrong_to_correct.get(word, word) for word in words]
-        return corrected_words
-
     def inference(self, sentence):
         words = sentence.split()
         cleaned_sentence = ' '.join(word if word in self.words_set else self.find_closest_word(word) for word in words)
         return cleaned_sentence
 
+class RegexModel:
+    def __init__(self, words_df_path):
+        self.words_df = pd.read_csv(words_df_path, encoding='utf-8', sep='\t')
+        self.wrong_to_correct = dict(zip(self.words_df['wrong'], self.words_df['correct']))
+        self.regex = self.compile_regex()
+
+    def compile_regex(self):
+        pattern = '|'.join(r'\b' + re.escape(wrong) + r'\b' for wrong in self.wrong_to_correct.keys())
+        return re.compile(pattern)
+
+    def correct_text(self, text):
+        def replace(match):
+            return self.wrong_to_correct[match.group(0)]
+        return self.regex.sub(replace, text)
+
 class CombinedModel:
-    def __init__(self, model_path, words_path):
-        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
-        self.model = AutoModelForCausalLM.from_pretrained(model_path)
-        self.model.resize_token_embeddings(len(self.tokenizer))
+    def __init__(self, model_path, words_path, words_df_path, use_gpt2=False):
+        self.use_gpt2 = use_gpt2
+        if self.use_gpt2:
+            self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+            self.model = AutoModelForCausalLM.from_pretrained(model_path)
+            self.model.resize_token_embeddings(len(self.tokenizer))
         self.distance_model = DistanceModel(words_path)
+        self.regex_model = RegexModel(words_df_path)
 
     def inference(self, sentence):
+
         cleaned_sentence = self.distance_model.inference(sentence)
-        input_ids = self.tokenizer.encode(f"<start> {cleaned_sentence} <end>", return_tensors='pt')
-        with torch.no_grad():
-            outputs = self.model.generate(input_ids, max_length=64, num_beams=5, early_stopping=True)
-        final_output = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-        
-        final_output_words = final_output.split()
+        corrected_sentence = self.regex_model.correct_text(cleaned_sentence)
+
+        if self.use_gpt2:
+            input_ids = self.tokenizer.encode(f"<start> {corrected_sentence} <end>", return_tensors='pt')
+            with torch.no_grad():
+                outputs = self.model.generate(input_ids, max_length=64, num_beams=5, early_stopping=True)
+            final_output = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+            final_output_words = final_output.split()
+        else:
+            final_output_words = corrected_sentence.split()
+
         input_words = sentence.split()
         if len(final_output_words) != len(input_words):
             final_output_words = final_output_words[:len(input_words)]
             if len(final_output_words) < len(input_words):
                 final_output_words += input_words[len(final_output_words):]
-        
+
         return ' '.join(final_output_words)
 
-model_path = "model.safetensors"
-words_path = "resources/big.txt"
+words_path = "resources/distinct_words.txt"
+words_df_path = "resources/created_words_dataset.txt"
+model = CombinedModel(model_path=None, words_path=words_path, words_df_path=words_df_path, use_gpt2=False)
 
-# model = DistanceModel(words_path)
-model = CombinedModel(model_path, words_path)
 
 class AcceptDirectMessageSkill(Skill):
     @match_event(UserInvite)
